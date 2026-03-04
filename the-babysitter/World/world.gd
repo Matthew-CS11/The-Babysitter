11aa195824d68f8984fcec7e36f362de5ef103af
Matthew-CS11 <2260213@flourbluffschools.org>
2026-03-04 13:11:19 -0600

    final commit

--- the-babysitter/World/world.gd
+++ the-babysitter/World/world.gd
@@ -9,10 +9,6 @@ extends NavigationRegion3D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	background_music.play()
-	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
 
 func _on_win_area_body_entered(body: Node3D) -> void:
 	if body is Player:
